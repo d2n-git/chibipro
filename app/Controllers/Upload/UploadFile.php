05048cd7e7a92239c8035ff1e9f6faeef26c8f8f
Ly Huynh <lyhuynh@deloitte.com>
2020-07-20 23:00:06 +0700

    created controler uploadfile

---
+++ app/Controllers/Upload/UploadFile.php
@@ -0,0 +1,16 @@
+<?php 
+namespace App\Controllers\Upload;
+use CodeIgniter\Controller;
+use App\Models\Users\CheckUser;
+
+class UploadFile extends Controller
+{
+    function index()
+    {
+       echo "page upload file";
+    }
+    function login()
+    {
+        
+    }
+}
