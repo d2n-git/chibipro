63b2d1bc258e0e06bfde6a5cf27cd12c240e3ef0
DESKTOP-O1T4U6B\dtanp <tanphu2871997@gmail.com>
2020-07-21 21:36:53 +0700

    merge

--- app/Views/templates/home.php
+++ app/Views/templates/home.php
@@ -1,3 +1,4 @@
+
 <section class="banner_part">
 <div class="container-fluid">
           <div class="container">
@@ -41,7 +42,7 @@
     </div>
 </section>
     <!-- new arrival part here -->
-    <section class="new_arrival section_padding">
+    <section class="new_arrival padding-5">
         <div class="container">
             <div class="row align-items-center">
                 <div class="col-lg-8">
@@ -55,186 +56,42 @@
             <div class="row">
                 <div class="col-lg-12">
                     <div class="new_arrival_iner filter-container">
-                        <div class="single_arrivel_item col-md-3">
-                            <img src="<?php echo base_url();?>/assets/img/3.jpg" alt="#">
-                            <div class="hover_text">
-                                <p>Canvas</p>
-                                <a href="single-product.html"><h3>Lorem Canvas Low-Top Sneaker</h3></a>
-                                <div class="rate_icon">
-                                    <a href="#"> <i class="fas fa-star"></i> </a>
-                                    <a href="#"> <i class="fas fa-star"></i> </a>
-                                    <a href="#"> <i class="fas fa-star"></i> </a>
-                                    <a href="#"> <i class="fas fa-star"></i> </a>
-                                    <a href="#"> <i class="fas fa-star"></i> </a>
-                                </div>
-                                <h5>$150</h5>
-                                <div class="social_icon">
-                                    <a href="#"><i class="ti-heart"></i></a>
-                                    <a href="#"><i class="ti-bag"></i></a>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="single_arrivel_item col-md-3">
-                            <img src="<?php echo base_url();?>/assets/img/3.jpg" alt="#">
-                            <div class="hover_text">
-                                <p>Canvas</p>
-                                <a href="single-product.html"><h3>Lorem Canvas Low-Top Sneaker</h3></a>
-                                <div class="rate_icon">
-                                    <a href="#"> <i class="fas fa-star"></i> </a>
-                                    <a href="#"> <i class="fas fa-star"></i> </a>
-                                    <a href="#"> <i class="fas fa-star"></i> </a>
-                                    <a href="#"> <i class="fas fa-star"></i> </a>
-                                    <a href="#"> <i class="fas fa-star"></i> </a>
-                                </div>
-                                <h5>$150</h5>
-                                <div class="social_icon">
-                                    <a href="#"><i class="ti-heart"></i></a>
-                                    <a href="#"><i class="ti-bag"></i></a>
+                        <?php foreach($pictures as $value){
+                        ?>
+                            <div class="single_arrivel_item col-md-3">
+                                <img src="<?php echo base_url();?>/assets/img/3.jpg" alt="#">
+                                <div class="hover_text">
+                                    <p>Canvas</p>
+                                    <a href="<?php echo base_url();?>/detail"><h3>Lorem Canvas Low-Top Sneaker</h3></a>
+                                    <div class="rate_icon">
+                                        <a href="#"> <i class="fas fa-star"></i> </a>
+                                        <a href="#"> <i class="fas fa-star"></i> </a>
+                                        <a href="#"> <i class="fas fa-star"></i> </a>
+                                        <a href="#"> <i class="fas fa-star"></i> </a>
+                                        <a href="#"> <i class="fas fa-star"></i> </a>
+                                    </div>
+                                    <h5>$150</h5>
+                                    <div class="social_icon">
+                                        <a href="#"><i class="ti-heart"></i></a>
+                                        <a href="#"><i class="ti-bag"></i></a>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                        <div class="single_arrivel_item  col-md-3" >
-                            <img src="<?php echo base_url();?>/assets/img/3.jpg" alt="#">
-                            <div class="hover_text">
-                                <p>Canvas</p>
-                                <a href="single-product.html"><h3>Lorem Canvas Low-Top Sneaker</h3></a>
-                                <div class="rate_icon">
-                                    <a href="#"> <i class="fas fa-star"></i> </a>
-                                    <a href="#"> <i class="fas fa-star"></i> </a>
-                                    <a href="#"> <i class="fas fa-star"></i> </a>
-                                    <a href="#"> <i class="fas fa-star"></i> </a>
-                                    <a href="#"> <i class="fas fa-star"></i> </a>
-                                </div>
-                                <h5>$150</h5>
-                                <div class="social_icon">
-                                    <a href="#"><i class="ti-heart"></i></a>
-                                    <a href="#"><i class="ti-bag"></i></a>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="single_arrivel_item  col-md-3">
-                            <img src="<?php echo base_url();?>/assets/img/3.jpg" alt="#">
-                            <div class="hover_text">
-                                <p>Canvas</p>
-                                <a href="single-product.html"><h3>Lorem Canvas Low-Top Sneaker</h3></a>
-                                <div class="rate_icon">
-                                    <a href="#"> <i class="fas fa-star"></i> </a>
-                                    <a href="#"> <i class="fas fa-star"></i> </a>
-                                    <a href="#"> <i class="fas fa-star"></i> </a>
-                                    <a href="#"> <i class="fas fa-star"></i> </a>
-                                    <a href="#"> <i class="fas fa-star"></i> </a>
-                                </div>
-                                <h5>$150</h5>
-                                <div class="social_icon">
-                                    <a href="#"><i class="ti-heart"></i></a>
-                                    <a href="#"><i class="ti-bag"></i></a>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="single_arrivel_item  col-md-3">
-                            <img src="<?php echo base_url();?>/assets/img/3.jpg" alt="#">
-                            <div class="hover_text">
-                                <p>Canvas</p>
-                                <a href="single-product.html"><h3>Lorem Canvas Low-Top Sneaker</h3></a>
-                                <div class="rate_icon">
-                                    <a href="#"> <i class="fas fa-star"></i> </a>
-                                    <a href="#"> <i class="fas fa-star"></i> </a>
-                                    <a href="#"> <i class="fas fa-star"></i> </a>
-                                    <a href="#"> <i class="fas fa-star"></i> </a>
-                                    <a href="#"> <i class="fas fa-star"></i> </a>
-                                </div>
-                                <h5>$150</h5>
-                                <div class="social_icon">
-                                    <a href="#"><i class="ti-heart"></i></a>
-                                    <a href="#"><i class="ti-bag"></i></a>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="single_arrivel_item  col-md-3">
-                            <img src="<?php echo base_url();?>/assets/img/3.jpg" alt="#">
-                            <div class="hover_text">
-                                <p>Canvas</p>
-                                <a href="single-product.html"><h3>Lorem Canvas Low-Top Sneaker</h3></a>
-                                <div class="rate_icon">
-                                    <a href="#"> <i class="fas fa-star"></i> </a>
-                                    <a href="#"> <i class="fas fa-star"></i> </a>
-                                    <a href="#"> <i class="fas fa-star"></i> </a>
-                                    <a href="#"> <i class="fas fa-star"></i> </a>
-                                    <a href="#"> <i class="fas fa-star"></i> </a>
-                                </div>
-                                <h5>$150</h5>
-                                <div class="social_icon">
-                                    <a href="#"><i class="ti-heart"></i></a>
-                                    <a href="#"><i class="ti-bag"></i></a>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="single_arrivel_item  col-md-3">
-                            <img src="<?php echo base_url();?>/assets/img/3.jpg" alt="#">
-                            <div class="hover_text">
-                                <p>Canvas</p>
-                                <a href="single-product.html"><h3>Lorem Canvas Low-Top Sneaker</h3></a>
-                                <div class="rate_icon">
-                                    <a href="#"> <i class="fas fa-star"></i> </a>
-                                    <a href="#"> <i class="fas fa-star"></i> </a>
-                                    <a href="#"> <i class="fas fa-star"></i> </a>
-                                    <a href="#"> <i class="fas fa-star"></i> </a>
-                                    <a href="#"> <i class="fas fa-star"></i> </a>
-                                </div>
-                                <h5>$150</h5>
-                                <div class="social_icon">
-                                    <a href="#"><i class="ti-heart"></i></a>
-                                    <a href="#"><i class="ti-bag"></i></a>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="single_arrivel_item  col-md-3">
-                            <img src="<?php echo base_url();?>/assets/img/3.jpg" alt="#">
-                            <div class="hover_text">
-                                <p>Canvas</p>
-                                <a href="single-product.html"><h3>Lorem Canvas Low-Top Sneaker</h3></a>
-                                <div class="rate_icon">
-                                    <a href="#"> <i class="fas fa-star"></i> </a>
-                                    <a href="#"> <i class="fas fa-star"></i> </a>
-                                    <a href="#"> <i class="fas fa-star"></i> </a>
-                                    <a href="#"> <i class="fas fa-star"></i> </a>
-                                    <a href="#"> <i class="fas fa-star"></i> </a>
-                                </div>
-                                <h5>$150</h5>
-                                <div class="social_icon">
-                                    <a href="#"><i class="ti-heart"></i></a>
-                                    <a href="#"><i class="ti-bag"></i></a>
-                                </div>
-                            </div>
-                        </div>
+                        <?php
+                        }
+                        ?>
                     </div>
                 </div> 
             </div>
         </div>
         <div class="container-fluid">
-        <div class="row justify-content-end">
-        <nav aria-label="Page navigation" >
-            <ul class="pagination">
-              <li class="page-item">
-                <a class="page-link" href="#" aria-label="Previous">
-                  <span aria-hidden="true">&laquo;</span>
-                  <span class="sr-only">Previous</span>
-                </a>
-              </li>
-              <li class="page-item"><a class="page-link" href="#">1</a></li>
-              <li class="page-item"><a class="page-link" href="#">2</a></li>
-              <li class="page-item"><a class="page-link" href="#">3</a></li>
-              <li class="page-item">
-                <a class="page-link" href="#" aria-label="Next">
-                  <span aria-hidden="true">&raquo;</span>
-                  <span class="sr-only">Next</span>
-                </a>
-              </li>
-            </ul>
-        </nav>
+        <div class="row justify-content-center">
+            <?= $pager->makeLinks($page, LIMITPICTURE, $total, 'template_picture'); ?>
         </div>
         </div>
     </section>
+
+
     <!-- new arrival part end -->
 
     <!-- free shipping here -->
@@ -251,9 +108,13 @@
             <div class="mt-10 col-12 col-md-6">
                 <div class="custom-file">
                     <input type="file" class="custom-file-input" id="inputGroupFile01"
-                    aria-describedby="inputGroupFileAddon01" class="single-input">
-                    <label class="custom-file-label" for="inputGroupFile01">Choose file</label>
+                    aria-describedby="inputGroupFileAddon01" class="single-input" onchange="readURL(this);">
+                    <label class="custom-file-label" for="inputGroupFile01" >Choose file</label>
+
                 </div>
+            </div>
+            <div class=" col-12 col-md-6" style="margin-top: 10px;" hidden id="areaupload">
+                    <img id="blah" src="<?php echo base_url();?>/assets/img/addiamge.PNG" />
             </div>
             <div class="mt-10 col-12 col-md-6">
                 <input type="email" name="EMAIL" placeholder="Email address" onfocus="this.placeholder = ''"
