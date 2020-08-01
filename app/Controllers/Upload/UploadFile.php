a633d3410a0ae6c7511629e3dfb8bbaade6e963f
Ly Huynh <lyhuynh@deloitte.com>
2020-08-01 12:46:21 +0700

    upload file

--- app/Controllers/Upload/UploadFile.php
+++ app/Controllers/Upload/UploadFile.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App\Controllers\Upload;
 
 use CodeIgniter\Controller;
@@ -26,20 +27,20 @@ class UploadFile extends Controller
         $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
 
         $reCaptcha = new ReCaptcha();
-		$response = null;
-		if ($this->request->getPost('g-recaptcha-response')) {
-			$response = $reCaptcha->verifyResponse(
-				$this->request->getServer('REMOTE_ADDR'),
-				$this->request->getPost('g-recaptcha-response')
-			);
+        $response = null;
+        if ($this->request->getPost('g-recaptcha-response')) {
+            $response = $reCaptcha->verifyResponse(
+                $this->request->getServer('REMOTE_ADDR'),
+                $this->request->getPost('g-recaptcha-response')
+            );
 
-			if ($response != null && $response->success) {
-				$uploadOk = 1;
-			} else {
+            if ($response != null && $response->success) {
+                $uploadOk = 1;
+            } else {
                 $MesError = "Click checkbox robot .";
                 $uploadOk = 0;
-			}
-		}
+            }
+        }
 
         // Check if image file is a actual image or fake image
         if (isset($_POST["submit"])) {
@@ -68,63 +69,78 @@ class UploadFile extends Controller
             $uploadOk = 0;
         }
         // Check if $uploadOk is set to 0 by an error
-        if ($uploadOk == 1) 
-        {
+        if ($uploadOk == 1) {
             $modelInsertUser = new InSertUserModel();
-            $insertPicture=new InSertPictureModel();
+            $insertPicture = new InSertPictureModel();
             $result = $insertPicture->GetMaxIdPictures();
             $nameNewPicture =  $result . '_' . basename($_FILES["fileToUpload"]["name"]);
-            try
-            {
-            if (move_uploaded_file($_FILES['fileToUpload']['tmp_name'], $target_dir .$nameNewPicture)) {
-                
-                $something = $this->request->getVar();
-                $resultUser = $modelInsertUser->GetUser($something['email']);
-                if (empty($resultUser)) 
-                {
-                    helper('text');
-                    $passWord=random_string('alnum', 16);
-                    $something['firstName'] = '';
-                    $something['lastName'] = '';
-                    $something['txtEmpPhone'] = '';
-                    $something['txtAddress'] = '';
-                    $something['password']=$passWord;
-                    $something['Permission'] = 2;
-                    $resultInsertUser=$modelInsertUser->InSertUsers($something);
-                    if($resultInsertUser)
-                    {
-                        $sendMail=new ConfigEmail();
-                        $sendMail->SendEmail('Password login :'.$passWord. ' Lest go link : https://www.google.com/','Send Password',$something['email']);
-                        $modelPicture['idUser'] = (int)$modelInsertUser->GetMaxIdUser();
+            try {
+                if (move_uploaded_file($_FILES['fileToUpload']['tmp_name'], $target_dir . $nameNewPicture)) {
+                    $something = $this->request->getVar();
+                    $resultUser = $modelInsertUser->GetUser($something['email']);
+                    if (empty($resultUser)) {
+                        helper('text');
+                        $passWord = random_string('alnum', 16);
+                        $something['firstName'] = '';
+                        $something['lastName'] = '';
+                        $something['txtEmpPhone'] = '';
+                        $something['txtAddress'] = '';
+                        $something['password'] = $passWord;
+                        $something['Permission'] = 2;
+                        $resultInsertUser = $modelInsertUser->InSertUsers($something);
+                        if ($resultInsertUser) {
+                            $sendMail = new ConfigEmail();
+                            $sendMail->SendEmail('Password login :' . $passWord . ' Lest go link : https://www.google.com/', 'Send Password', $something['email']);
+                            $modelPicture['idUser'] = (int)$modelInsertUser->GetMaxIdUser();
+                            $modelPicture['idStatusPicture'] = 1;
+                            $modelPicture['Name'] = $nameNewPicture;
+                            $modelPicture['NumberLike'] = 0;
+                            $modelPicture['DateUp'] = date('Y-m-d h:m:s');
+                            $modelPicture['StatusSendEmail'] = 0;
+                            $resultInsertPicture = $insertPicture->InSertPicture($modelPicture);
+                            if ($resultInsertPicture) {
+                                $MesError = 'uploaded success';
+                                $json = ["message" => $MesError, "status" => $uploadOk];
+                                echo json_encode($json);
+                            } else {
+                                $MesError = 'uploaded Failed';
+                                unlink($target_dir . $nameNewPicture);
+                                $json = ["message" => $MesError, "status" => $uploadOk];
+                                echo json_encode($json);
+                            }
+                        } else {
+                            $MesError = 'uploaded Failed';
+                            unlink($target_dir . $nameNewPicture);
+                            $json = ["message" => $MesError, "status" => $uploadOk];
+                            echo json_encode($json);
+                        }
+                    } else {
+                        $modelPicture['idUser'] = (int)$resultUser['idUser'];
                         $modelPicture['idStatusPicture'] = 1;
                         $modelPicture['Name'] = $nameNewPicture;
                         $modelPicture['NumberLike'] = 0;
                         $modelPicture['DateUp'] = date('Y-m-d h:m:s');
                         $modelPicture['StatusSendEmail'] = 0;
-                        $resultInsertPicture=$insertPicture->InSertPicture($modelPicture);
+                        $resultInsertPicture = $insertPicture->InSertPicture($modelPicture);
+                        if ($resultInsertPicture) {
+                            $MesError = 'uploaded success';
+                            $json = ["message" => $MesError, "status" => $uploadOk];
+                            echo json_encode($json);
+                        } else {
+                            $MesError = 'uploaded Failed';
+                            unlink($target_dir . $nameNewPicture);
+                            $json = ["message" => $MesError, "status" => $uploadOk];
+                            echo json_encode($json);
+                        }
                     }
-                    
                 }
-                else
-                {
-                    $modelPicture['idUser'] = (int)$resultUser['idUser'];
-                    $modelPicture['idStatusPicture'] = 1;
-                    $modelPicture['Name'] = $nameNewPicture;
-                    $modelPicture['NumberLike'] = 0;
-                    $modelPicture['DateUp'] = date('Y-m-d h:m:s');
-                    $modelPicture['StatusSendEmail'] = 0;
-                    $resultInsertPicture=$insertPicture->InSertPicture($modelPicture); 
-                }
-            }
-            }catch(Exception $e)
-             {
-                $json = ["message"=>$MesError , "status" => $uploadOk];
+            } catch (Exception $e) {
+                $json = ["message" => $MesError, "status" => $uploadOk];
                 echo json_encode($json);
-             }
-        }else{
-            $json = ["message"=>$MesError , "status" => $uploadOk];
-             echo json_encode($json);
+            }
+        } else {
+            $json = ["message" => $MesError, "status" => $uploadOk];
+            echo json_encode($json);
         }
-        
     }
 }
