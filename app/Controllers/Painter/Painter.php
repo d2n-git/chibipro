5cf9e7175d15f0ba3c17f268731715ba23e13ef4
Ly Huynh <lyhuynh@deloitte.com>
2020-09-28 22:26:17 +0700

    update

--- app/Controllers/Painter/Painter.php
+++ app/Controllers/Painter/Painter.php
@@ -3,6 +3,8 @@ namespace App\Controllers\Painter;
 
 use CodeIgniter\Controller;
 use App\Models\PictureModel;
+use App\Libraries\alert;
+
 class Painter extends Controller
 {
     function Index()
@@ -16,7 +18,14 @@ class Painter extends Controller
 		$data['total'] = count($pictureModel->getAllPictureCount());
 		$data['pager'] = $pager;
 		$data['pictures'] = $pictures;
-        $data['viewchild'] = './painter/content';
-        return view('templates/base_view', $data);
+		$data['viewchild'] = './painter/content';
+		$session = \Config\Services::session();
+		if(isset($_SESSION['logged_in']) && $_SESSION['logged_in']) return view('templates/base_view', $data);
+		else 
+		{
+			$aler = new alert();
+			$aler->alert('Please login user!');
+			return redirect()->to('/Users/Login');
+		}
     }
 }
