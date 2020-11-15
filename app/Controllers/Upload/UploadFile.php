eec95c1a62a1c9eb14ba10d01a3e976a5a669f6b
Ly Huynh <lyhuynh@deloitte.com>
2020-11-15 11:53:59 +0700

    update check login when enter page by  url

--- app/Controllers/Upload/UploadFile.php
+++ app/Controllers/Upload/UploadFile.php
@@ -151,6 +151,12 @@ class UploadFile extends Controller
     }
 
     function detail(){
+        $session = \Config\Services::session();
+		if(!isset($_SESSION['logged_in']))
+		{
+			return redirect() -> to(base_url('/Users/Login'));
+		}
+		else if (!$_SESSION['logged_in']) return redirect() -> to(base_url('/Users/Login'));
         $id = $this->request->getGet('id');
         $modePicture = new InSertPictureModel();
         $data['Picture'] = $modePicture->GetPictureById($id);
